cf10f12dc01d9cc280ef9b4d3f7697c4d1931555
userbiin <162470759+userbiin@users.noreply.github.com>
2025-11-03 06:38:44 +0000

    templates demo 1

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello world")
+from flask import Flask, Blueprint
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('templates/index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
